34f50c6e684e7f0cce96c6f251b03cf68b4b27c6
hao.chen37 <hao.chen37@cevt.se>
2022-11-04 17:45:54 +0800

    add domain

--- analytic_advance/models/account_analytic_default_ext.py
+++ analytic_advance/models/account_analytic_default_ext.py
@@ -1,6 +1,7 @@
 import logging
 from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError, UserError
+from odoo.osv import expression
 
 _logger = logging.getLogger(__name__)
 
@@ -9,26 +10,34 @@ class AccountAnalyticDefaultExt(models.Model):
     _inherit = "account.analytic.default"
 
     first_name = fields.Char(string='操作数1名称')
-    first_type = fields.Selection([('model','模型'),('api','接口'),('pre','上级结果')],string="操作数1类型",default="model")
-    first_model = fields.Many2one('ir.model',string="模型")
+    first_type = fields.Selection(
+        [('model', '模型'), ('api', '接口'), ('pre', '上级结果')], string="操作数1类型", default="model")
+    first_model = fields.Many2one('ir.model', string="模型")
     first_api = fields.Char(string='接口')
     first_field_name = fields.Char(string='操作数1字段名称')
-    first_field_config_name = fields.Many2one('field.config.table',string='操作数1字段名称')
+    first_field_config_name = fields.Many2one(
+        'field.config.table', string='操作数1字段名称')
+    field_domain = fields.Char(
+        string='范围', compute='_compute_field_domain',
+        readonly=False, store=True)
 
     second_name = fields.Char(string='操作数2名称')
-    second_type = fields.Selection([('model','模型'),('api','接口'),('pre','上级结果')],string="操作数2类型",default="model")
-    second_model = fields.Many2one('ir.model',string="模型")
+    second_type = fields.Selection(
+        [('model', '模型'), ('api', '接口'), ('pre', '上级结果')], string="操作数2类型", default="model")
+    second_model = fields.Many2one('ir.model', string="模型")
     second_api = fields.Char(string='接口')
     second_field_name = fields.Char(string='操作数2字段名称')
-    second_field_config_name = fields.Many2one('field.config.table',string='操作数2字段名称')
-    operator = fields.Selection([('add','＋'),('reduce','－'),('multiply','×'),('divide','÷')],string="操作符")
+    second_field_config_name = fields.Many2one(
+        'field.config.table', string='操作数2字段名称')
+    operator = fields.Selection(
+        [('add', '＋'), ('reduce', '－'), ('multiply', '×'), ('divide', '÷')], string="操作符")
 
     @api.onchange('first_field_config_name')
     def _onchange_first_field_config_name(self):
         for record in self:
             record.first_model = record.first_field_config_name.model_name.id
             record.first_field_name = record.first_field_config_name.field_name.name
-    
+
     @api.onchange('second_field_config_name')
     def _onchange_second_field_config_name(self):
         for record in self:
@@ -38,13 +47,33 @@ class AccountAnalyticDefaultExt(models.Model):
     @api.onchange('first_type')
     def _onchange_first_type(self):
         for record in self:
-            record.first_model=""
-            record.first_field_name=""
-            record.first_field_config_name=""
-    
+            record.first_model = ""
+            record.first_field_name = ""
+            record.first_field_config_name = ""
+
     @api.onchange('second_type')
     def _onchange_second_type(self):
         for record in self:
-            record.second_model=""
-            record.second_field_name=""
-            record.second_field_config_name=""
+            record.second_model = ""
+            record.second_field_name = ""
+            record.second_field_config_name = ""
+
+    def _get_default_field_domain(self):
+        field_domain = []
+        # if self.mailing_model_name == 'mailing.list' and self.contact_list_ids:
+        #     field_domain = [('list_ids', 'in', self.contact_list_ids.ids)]
+
+        # if self.mailing_type == 'mail' and 'is_blacklisted' in self.env[self.mailing_model_name]._fields:
+        #     field_domain = expression.AND(
+        #         [[('is_blacklisted', '=', False)], mailing_domain])
+
+        return field_domain
+
+    @api.depends('first_model')
+    def _compute_field_domain(self):
+        for _field in self:
+            if not _field.first_model:
+                _field.field_domain = ''
+            else:
+                _field.field_domain = repr(
+                    _field._get_default_field_domain())
